5769aafa5acd8a664787ad58a078808818c7d365
Steven Akins <stevenakins@stevens-mbp.attlocal.net>
2020-01-19 16:24:29 -0500

    progress

---
+++ src/Progress/index.tsx
@@ -0,0 +1,6 @@
+import React, { Fragment } from 'react';
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const RevelProgress: React.FC = () => <><h1>Revel typescipt stuff loading!...</h1><CircularProgress /></>
+
+export default RevelProgress;
---
+++ stories/progress.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import RevelProgress from '../public/Progress';
+
+export default {
+  title: 'Revel Progress',
+  component: RevelProgress,
+};
+
+export const Progress = () => <RevelProgress />;
