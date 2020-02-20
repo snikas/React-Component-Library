252d58f761c49b5d3971ced126919dce00d4e69c
Steven Akins <stevenakins@stevens-mbp.attlocal.net>
2020-02-20 15:57:53 -0500

    update react-dom

--- src/Progress/index.tsx
+++ src/Progress/index.tsx
@@ -1,6 +1,6 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import CircularProgress from "@material-ui/core/CircularProgress";
 
-const RevelProgress: React.FC = () => <><h1>Revel typescipt stuff loading!...</h1><CircularProgress /></>
+const RevelProgress: React.FC = () => <><h1>Revel tyipt!...</h1><CircularProgress /></>
 
 export default RevelProgress;
