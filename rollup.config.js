d8b44ac66535b960d2b6188b7a990b29ac8ff0f4
steven Akins <steven.akins@revelsystems.com>
2020-02-24 15:42:49 -0500

    add build support

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from "rollup-plugin-terser";
+import pkg from './package.json';
+
+export default [
+	{
+		input: 'src/index.ts',
+		plugins: [
+			typescript(),
+			commonjs({
+                namedExports: {
+                    'react-is': ['ForwardRef'],
+                    'prop-types': ["elementType"]
+                }
+            }),
+            resolve(),
+            terser()
+		],
+		output: [
+			{ file: pkg.main, format: 'cjs', sourcemap: true },
+			{ file: pkg.module, format: 'es' },
+        ],
+        external: ["react", "react-dom"]
+	},
+];
